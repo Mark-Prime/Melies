b8b728b610e89a731595e992d335456bbbf12c3e
Mark-Prime <msmarkieer@gmail.com>
2024-04-22 17:45:36 -0500

    removed no longer working options

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -44,6 +44,24 @@
       class="settings__input input"
     />
   </div>
+  {#if output_settings.method === "sparklyfx"}
+    <div class="settings__input-group settings__span">
+      <label
+        for="output_folder"
+        class="settings__label tooltip"
+        data-tooltip="The full filepath to the folder you want to output to.
+If left blank, the output folder will default to your sparklyfx settings."
+        style={`--kills: 1;`}
+      >
+        Output Folder
+      </label>
+      <input
+        bind:value={output_settings.folder}
+        id="output_folder"
+        class="settings__input input"
+      />
+    </div>
+  {/if}
   <div class="settings__switch">
     <label class="switch">
       <input type="checkbox" bind:checked={settings.safe_mode} />
@@ -93,17 +111,20 @@
       for="method"
       class="settings__label tooltip"
       data-tooltip={`The method you want to use to record each clip:
-      h264 - QuickTime encoding to .mp4
-      jpeg - generates .jpeg image sequence
+      h264 - QuickTime encoding to .mp4 (requires you to launch the 32bit tf.exe)
       tga - generates .tga image sequence
-      SVR - uses Source Video Render to record .mkv
-      SVR.mov - uses Source Video Render to record .mov
-      SVR.mp4 - uses Source Video Render to record .mp4 (Requires SVR encoder to be updated)
+      jpeg - generates .jpeg image sequence
+      SparklyFX - Automates recording using SparklyFX
       Lawena - uses commands for Lawena (settings will be overwritten by Lawena)
       Do Not Record - ignores recording commands but still skips
 `}
-      style={`--kills: 8;`}
+      style={`--kills: 6;`}
     >
+    <!-- 
+      NO LONGER SUPPORTED
+      SVR - uses Source Video Render to record .mkv
+      SVR.mov - uses Source Video Render to record .mov
+      SVR.mp4 - uses Source Video Render to record .mp4 (Requires SVR encoder to be updated) -->
       Recording Method
     </label>
     <select
@@ -112,11 +133,12 @@
       class="settings__input input--sec"
     >
       <option value="h264">h264</option>
-      <option value="jpeg">jpeg</option>
       <option value="tga">tga</option>
-      <option value="svr">SVR</option>
+      <option value="jpeg">jpeg</option>
+      <option value="sparklyfx">SparklyFX</option>
+      <!-- <option value="svr">SVR</option>
       <option value="svr.mov">SVR.mov</option>
-      <option value="svr.mp4">SVR.mp4</option>
+      <option value="svr.mp4">SVR.mp4</option> -->
       <option value="lawena">Lawena</option>
       <option value="none">Do Not Record</option>
     </select>
