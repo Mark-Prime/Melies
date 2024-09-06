219e93feed14e9406713ef361b1f109254780680
Mark-Prime <msmarkieer@gmail.com>
2024-09-05 23:42:46 -0500

    separated files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use addons::compile_addons;
 use chrono::prelude::*;
 use regex::Regex;
 use serde_json::{ self, json, Value };
 use std::fs::{ DirEntry, File };
 use std::io::Write;
 use std::path::Path;
-use std::process::Command;
 use std::{ env, fs };
 use tauri::command;
 use vdm::action::ActionType;
@@ -18,6 +18,7 @@ use crate::event::Event;
 use crate::event::EventStyle::{ Bookmark, Killstreak };
 use crate::logstf::parse;
 use crate::vdms::*;
+use crate::macros::*;
 
 mod clip;
 mod demos;
@@ -25,22 +26,8 @@ mod event;
 mod settings;
 mod vdms;
 mod logstf;
-
-macro_rules! ifelse {
-    ($c:expr, $v:expr, $v1:expr) => {
-        if $c {
-            $v
-        } else {
-            $v1
-        }
-    };
-}
-
-macro_rules! extend {
-    ($v:expr, $v1:expr, $c:expr) => {
-        $v = format!("{}{}", $v, format!($v1, $c))
-    };
-}
+mod addons;
+mod macros;
 
 fn setting_as_bin(setting: &Value) -> i64 {
     if !setting.is_boolean() {
@@ -75,119 +62,6 @@ fn setting_as_bool(setting: &Value) -> bool {
             }
         _ => false,
     }
-}
-
-fn compile_addon_settings(v: &Value, depth: usize) -> String {
-    let mut cfg = "".to_string();
-
-    let v_map = match v.as_object() {
-        Some(v_map) => v_map,
-        None => {
-            return cfg;
-        }
-    };
-
-    let tab_depth = "\t".repeat(depth);
-
-    let mut vec = v_map.iter().collect::<Vec<(&String, &Value)>>();
-
-    vec.sort_by(|a, b| (a.1["type"] == "group").cmp(&(b.1["type"] == "group")));
-
-    for (ki, vi) in vec {
-        if vi["ignoreIfDefault"] == true && vi["value"] == vi["default"] {
-            continue;
-        }
-
-        match &vi["type"] {
-            Value::String(vi_type) =>
-                match vi_type.as_str() {
-                    "group" => {
-                        let addon_text = compile_addon_settings(&vi["settings"], depth + 1);
-
-                        if addon_text.is_empty() {
-                            continue;
-                        }
-
-                        extend!(cfg, "\r\n{}\t", tab_depth);
-                        extend!(cfg, "\\\\ {}\r\n", ki.as_str());
-                        extend!(cfg, "{}", compile_addon_settings(&vi["settings"], depth + 1));
-                    }
-                    "toggle" => {
-                        if vi["value"] == true {
-                            extend!(cfg, "{};\r\n", tab_depth.clone() + vi["command"].as_str().unwrap());
-                        }
-                    }
-                    "bool" => {
-                        extend!(
-                            cfg,
-                            "{};\r\n",
-                            format!(
-                                "{}{} {}",
-                                tab_depth.clone(),
-                                vi["command"].as_str().unwrap(),
-                                setting_as_bin(&vi["value"])
-                            )
-                        );
-                    }
-                    "string" | "int" => {
-                        if vi["ignoreIfBlank"] == true && vi["value"] == "" {
-                            continue;
-                        }
-
-                        extend!(
-                            cfg,
-                            "{};\r\n",
-                            format!(
-                                "{}{} {}",
-                                tab_depth.clone(),
-                                vi["command"].as_str().unwrap(),
-                                vi["value"]
-                            )
-                        );
-                    }
-                    _ => {
-                        continue;
-                    }
-                }
-            _ => {
-                continue;
-            }
-        }
-    }
-
-    return cfg;
-}
-
-fn compile_addons(settings: &Value) -> String {
-    let addons = settings["addons"].as_object();
-
-    let mut cfg = "".to_string();
-
-    if addons.is_none() {
-        return cfg;
-    }
-
-    let addons = addons.unwrap();
-
-    let iter = addons.into_iter();
-
-    let mut vec = iter.collect::<Vec<(&String, &Value)>>();
-
-    vec.sort_by(|a, b| (a.1["type"] == "group").cmp(&(b.1["type"] == "group")));
-    
-    for (k, v) in addons {
-        let addon_text = compile_addon_settings(v, 0);
-        
-        if addon_text.is_empty() {
-            continue;
-        }
-
-        extend!(cfg, "\r\necho \"Running {} addon\";\r\n", k);
-
-        extend!(cfg, "{}\r\n", addon_text);
-    }
-
-    return cfg;
 }
 
 fn write_cfg(settings: &Value) -> Result<(), String> {
@@ -1130,29 +1004,8 @@ fn save_vdm(name: Value, vdm: Value) -> Value {
 }
 
 #[command]
-fn open_addons_folder() -> Value {
-    let user_profile = env::var("USERPROFILE");
-
-    let addons_path = match user_profile {
-        Ok(profile) => { format!("{}\\Documents\\Melies\\addons", profile) }
-        Err(_) => {
-            format!(
-                "{}\\addons",
-                std::env::current_exe().unwrap().parent().unwrap().to_str().unwrap()
-            )
-        }
-    };
-
-    fs::create_dir_all(&addons_path).unwrap();
-
-    Command::new("explorer")
-        .arg(addons_path)
-        .spawn()
-        .unwrap();
-
-    json!({
-        "success": true
-    })
+fn open_addons_folder() {
+    addons::open_addons_folder();
 }
 
 fn main() {
