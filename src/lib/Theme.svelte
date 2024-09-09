b1273ead32dbb85e32eb082211c70f43b5243297
Mark-Prime <msmarkieer@gmail.com>
2024-09-09 18:01:39 -0500

    better matched the actual input

--- src/lib/Theme.svelte
+++ src/lib/Theme.svelte
@@ -7,36 +7,36 @@
       <h4>Primary Colors</h4>
       <div class="colors">
         <div class="color_display tooltip" data-tooltip="pri" style="--color: var(--pri); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="pri-text" style="--color: var(--pri-text); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="pri-con" style="--color: var(--pri-con); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="pri-con-text" style="--color: var(--pri-con-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="pri_text" style="--color: var(--pri-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="pri_con" style="--color: var(--pri-con); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="pri_con_text" style="--color: var(--pri-con-text); --kills: 0;"></div>
       </div>
     </div>
     <div>
       <h4>Secondary Colors</h4>
       <div class="colors">
         <div class="color_display tooltip" data-tooltip="sec"  style="--color: var(--sec); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="sec-text" style="--color: var(--sec-text); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="sec-con" style="--color: var(--sec-con); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="sec-con-text" style="--color: var(--sec-con-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="sec_text" style="--color: var(--sec-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="sec_con" style="--color: var(--sec-con); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="sec_con_text" style="--color: var(--sec-con-text); --kills: 0;"></div>
       </div>
     </div>
     <div>
       <h4>Tertiary Colors</h4>
       <div class="colors">
         <div class="color_display tooltip" data-tooltip="tert" style="--color: var(--tert); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="tert-text" style="--color: var(--tert-text); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="tert-con" style="--color: var(--tert-con); --kills: 0;"></div>
-        <div class="color_display tooltip tooltip--left" data-tooltip="tert-con-text" style="--color: var(--tert-con-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="tert_text" style="--color: var(--tert-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="tert_con" style="--color: var(--tert-con); --kills: 0;"></div>
+        <div class="color_display tooltip tooltip--left" data-tooltip="tert_con_text" style="--color: var(--tert-con-text); --kills: 0;"></div>
       </div>
     </div>
     <div>
       <h4>Error Colors</h4>
       <div class="colors">
         <div class="color_display tooltip" data-tooltip="err" style="--color: var(--err); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="err-text" style="--color: var(--err-text); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="err-con" style="--color: var(--err-con); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="err-con-text" style="--color: var(--err-con-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="err_text" style="--color: var(--err-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="err_con" style="--color: var(--err-con); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="err_con_text" style="--color: var(--err-con-text); --kills: 0;"></div>
       </div>
     </div>
     <div>
@@ -44,7 +44,7 @@
       <div class="colors">
         <div class="color_display tooltip" data-tooltip="bg" style="--color: var(--bg); --kills: 0;"></div>
         <div class="color_display tooltip" data-tooltip="bg2" style="--color: var(--bg2); --kills: 0;"></div>
-        <div class="color_display tooltip" data-tooltip="bg-text" style="--color: var(--bg-text); --kills: 0;"></div>
+        <div class="color_display tooltip" data-tooltip="bg_text" style="--color: var(--bg-text); --kills: 0;"></div>
       </div>
     </div>
     <div>
