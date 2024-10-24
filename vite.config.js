ddca3f917ce899a4c2ebe1b2a68a8b11a36c62c7
Mark-Prime <msmarkieer@gmail.com>
2024-10-24 01:24:50 -0500

    Removed scss warning

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern' // or "modern"
+      }
+    }
+  },
+
   plugins: [sveltekit()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
