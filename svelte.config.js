0620a9cd0d2b4420a268f0f2d4f7226cf63d9ff6
Mark-Prime <msmarkieer@gmail.com>
2025-05-13 16:07:55 -0500

    Updated dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import staticAdapter from "@sveltejs/adapter-static";
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: sveltePreprocess(),
   kit: {
     adapter: staticAdapter(),
   },
