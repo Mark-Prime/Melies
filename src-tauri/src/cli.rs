28dc2588b72e8480f23747011ff2131a044a4932
Mark-Prime <msmarkieer@gmail.com>
2025-11-13 22:44:02 -0600

    Updated how the cli is called

--- src-tauri/src/cli.rs
+++ src-tauri/src/cli.rs
@@ -1,85 +1,251 @@
-use std::{ fs, path::Path };
+use std::{ fs, path::Path, vec };
 
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 use crate::{ demos::scan_demo, event::{ Event, EventStyle }, events };
 
-pub fn run_cli() -> bool {
-  let args: Vec<String> = std::env::args().collect();
+#[derive(Debug)]
+enum Commands {
+  Help,
+  Error(String),
+  Run {
+    options: Map<String, Value>
+  },
+  LaunchTf2 {
+    options: Map<String, Value>
+    // --preset
+    // --demo
+  },
+  GrabEvents {
+    demo: String,
+    options: Map<String, Value>
+    // --player
+  },
+  OpenDemo {
+    demo: String,
+    options: Map<String, Value>
+    // --output
+  },
+}
 
-  if args.len() < 2 {
-    return true;
+impl Commands {
+  fn new(mut args: Vec<String>) -> Self {
+
+    args.remove(0);
+
+    let options = Self::get_options(args.clone());
+
+    match args[0].as_str().to_lowercase().as_str() {
+      "help" | "--help"  => Commands::Help,
+      "run" => Commands::Run {
+        options
+      },
+      "launchtf2" => Commands::LaunchTf2 {
+        options
+      },
+      "events" => {
+        if args.len() < 2 {
+          return Commands::Error("Missing <demo path>".to_string());
+        }
+        Commands::GrabEvents {
+          demo: args[1].clone(),
+          options
+        }
+      },
+      "open" => {
+        if args.len() < 2 {
+          return Commands::Error("Missing <demo path>".to_string());
+        }
+
+        Commands::OpenDemo {
+          demo: args[1].clone(),
+          options
+        }
+      },
+      _ => Commands::Error(format!("Unknown command: {}", args[1])),
+    }
   }
 
-  let pattern = args[1].clone();
+  fn get_options(args: Vec<String>) -> Map<String, Value> {
+    let mut options = Map::new();
 
-  let res = match pattern.as_str() {
-    "--run" | "-R" => run(),
-    "--events" | "-E" => grab_events(args),
-    "--launchTf2" | "-L" | "--launchtf2" => launch_tf2(args),
-    "--open" | "-O" => open_demo(args),
-    "--help" | "-H" => {
-      println!("Key:");
-      println!("  <> required argument");
-      println!("  [] optional argument");
+    for (i, mut arg) in args.iter().skip(1).enumerate() {
+      if !arg.contains("--") {
+        continue; 
+      }
+
+      let binding = arg.clone().replace("--", "");
+      arg = &binding;
+
+      if args.len() > i + 2 && !args[i + 2].is_empty() {
+        if args[i + 2].contains("--") {
+          options.insert(arg.clone(), Value::Bool(true));
+          continue;
+        }
+
+        options.insert(arg.clone(), Value::String(args[i + 2].clone()));
+      } else {
+        options.insert(arg.clone(), Value::Bool(true));
+      }
+    }
+
+    options
+  }
+
+  fn options(&self) -> Map<String, Value> {
+    match self {
+      Commands::Run { options } => options.to_owned(),
+      Commands::LaunchTf2 { options } => options.to_owned(),
+      Commands::GrabEvents { options, .. } => options.to_owned(),
+      Commands::OpenDemo { options, .. } => options.to_owned(),
+      _ => Map::new(),
+    }
+  }
+  fn run(&self) -> Option<Value> {
+    match self {
+      Commands::Run { options } => Some(run(options.clone())),
+      Commands::LaunchTf2 { options } => Some(launch_tf2(options.clone())),
+      Commands::GrabEvents { demo, options } => Some(grab_events(demo.to_string(), options.clone())),
+      Commands::OpenDemo { demo, options } => Some(open_demo(demo.to_string(), options.clone())),
+      Commands::Help => {
+        self.print_help();
+        return None;
+      }
+      Commands::Error(msg) => {
+        println!("{}", msg);
+        return None;
+      }
+    }
+  }
+
+  fn print_help(&self) {
+    let mut function = String::from("");
+    let mut commands = vec!["help", "run", "launchtf2", "events <demo path>", "open <demo path>"];
+    let mut args: Vec<&str> = vec![];
+    let mut optional_args: Vec<&str> = vec![];
+
+    match self {
+      Commands::Run { options: _ } => {
+        function = "Runs Méliès and builds the VDMs.".to_string();
+        commands = vec![];
+        args = vec![];
+        optional_args = vec![];
+      },
+      Commands::LaunchTf2 { options: _ } => {
+        function = "Launches Team Fortress 2.".to_string();
+        commands = vec![];
+        args = vec![];
+        optional_args = vec!["--preset <preset>", "--demo <demo name>"];
+      },
+      Commands::GrabEvents { demo: _, options: _ } => {
+        function = "Scrapes events from a demo using the demo parser\n\t\t  and exports them to the _events.txt file.".to_string();
+        commands = vec![];
+        args = vec!["<demo path>"];
+        optional_args = vec!["--player <player name or steamid64>"];
+      },
+      Commands::OpenDemo { demo: _, options: _ } => {
+        function = "Scrapes all demo information and outputs it to\n\t\t  the console or a specified output path.".to_string();
+        commands = vec![];
+        args = vec!["<demo path>"];
+        optional_args = vec!["--output <output path>"];
+      },
+      _ => {},
+    };
+
+    if function.len() > 0 {
+      println!("Function: {}", function);
       println!("");
+    }
+    
+    if commands.len() > 0 {
       println!("Commands:");
-      println!("  -R, --run");
-      println!("  -E, --events <demo path> [player]");
-      println!("  -L, --launchTf2 [preset] [demo name]");
-      println!("  -O, --open <demo path> [output path]");
-      return false;
+      for command in commands {
+        println!("  {}", command);
+      }
+      println!("");
     }
-    _ => {
-      println!("Unknown command");
-      return true;
-    }
-  };
 
-  res
+    if args.len() > 0 {
+      println!("Arguments:");
+      for arg in args {
+        println!("  {}", arg);
+      }
+    }
+
+    if optional_args.len() == 0 {
+      return;
+    }
+
+    println!("Optional Arguments:");
+    for arg in optional_args {
+      println!("  {}", arg);
+    }
+  }
 }
 
-fn run() -> bool {
-  println!("Running");
-
-  crate::ryukbot::run_ryukbot();
-
-  println!("Done");
-
-  false
+impl std::fmt::Display for Commands {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Commands::Run { .. } => write!(f, "run"),
+      Commands::LaunchTf2 { .. } => write!(f, "launchtf2"),
+      Commands::GrabEvents { .. } => write!(f, "events"),
+      Commands::OpenDemo { .. } => write!(f, "open"),
+      _ => write!(f, "help"),
+    }
+  }
 }
 
-fn open_demo(args: Vec<String>) -> bool {
-  println!("Opening demo");
+fn print_verbose(options: &Map<String, Value>, msg: &str) {
+  let verbose = options.get("verbose").is_some();
 
-  if args.len() < 3 {
-    println!("Missing demo path");
-    return false;
+  if verbose {
+    println!("{}", msg);
+  }
+}
+
+fn run(options: Map<String, Value>) -> Value {
+  print_verbose(&options, "Running");
+
+  let res = crate::ryukbot::run_ryukbot();
+
+  if res.get("err_text").is_some() {
+    print_verbose(&options, format!("\nError code: {}\n", res["code"]).as_str());
+    print_verbose(&options, format!("{}", res["err_text"].as_str().unwrap()).as_str());
+    return res;
   }
 
-  let demo_path = args[2].clone();
+  print_verbose(&options, "Success");
+
+  print_verbose(&options, "\nStats:");
+  print_verbose(&options, format!("  Events: {}", res["events"]).as_str());
+  print_verbose(&options, format!("  Clips: {}", res["clips"]).as_str());
+  print_verbose(&options, format!("  VDMs: {}\n", res["vdms"]).as_str());
+
+  print_verbose(&options, "First Demo:");
+
+  println!("{}", res["first_demo"].as_str().unwrap());
+
+  return res;
+}
+
+fn open_demo(demo_path: String, options: Map<String, Value>) -> Value {
   let settings = crate::settings::load_settings();
 
   let res = scan_demo(settings, demo_path);
 
-  if args.len() < 4 {
-    return false;
+  if options.get("output").is_none() {
+    return Value::from(res);
   }
 
-  let file = fs::File::create(args[3].clone()).unwrap();
+  let file = fs::File::create(options["output"].as_str().unwrap()).unwrap();
 
   let writer = std::io::BufWriter::new(file);
   serde_json::to_writer_pretty(writer, &res).unwrap();
 
-  false
+  Value::from(res)
 }
 
-fn grab_events(args: Vec<String>) -> bool {
-  if args.len() < 3 {
-    println!("Missing demo path");
-    return false;
-  }
-
+fn grab_events(demo: String, options: Map<String, Value>) -> Value {
   let settings = crate::settings::load_settings();
   let automation_settings = &settings["automation"];
 
@@ -91,13 +257,11 @@ fn grab_events(args: Vec<String>) -> bool {
     std::io::stdin().read_line(&mut input).unwrap();
 
     if input.trim().to_lowercase() != "y" {
-      return false;
+      return Value::Null;
     }
   }
 
-  println!("Scanning demo");
-
-  let demo = args[2].clone();
+  print_verbose(&options, "Scanning demo");
   let binding = demo.clone();
   let demo_path = Path::new(&binding);
 
@@ -105,8 +269,8 @@ fn grab_events(args: Vec<String>) -> bool {
 
   let mut player_id = String::new();
 
-  if args.len() > 3 && !args[3].is_empty() {
-    player_id = get_player_id(args[3].clone(), &res);
+  if options.get("player").is_some() {
+    player_id = get_player_id(options["player"].as_str().unwrap().to_string(), &res);
   }
 
   let player_lives = res["data"]["player_lives"].as_object().unwrap();
@@ -197,9 +361,7 @@ fn grab_events(args: Vec<String>) -> bool {
 
   events.sort_by(|a, b| a.tick.cmp(&b.tick));
 
-  events::directly_save_events(events);
-
-  false
+  events::directly_save_events(events)
 }
 
 fn get_player_id(arg: String, res: &serde_json::Value) -> String {
@@ -225,28 +387,27 @@ fn get_player_id(arg: String, res: &serde_json::Value) -> String {
   String::new()
 }
 
-fn launch_tf2(args: Vec<String>) -> bool {
+fn launch_tf2(options: Map<String, Value>) -> Value {
   let mut tab = String::from("0");
-  let mut demo_name = String::new();
 
-  if args.len() > 2 && !args[2].is_empty() {
-    if args[2].chars().all(char::is_numeric) {
-      tab = args[2].clone();
+  if options.get("preset").is_some() {
+    if options["preset"].as_str().unwrap().chars().all(char::is_numeric) {
+      tab = options["preset"].as_str().unwrap().to_string();
     } else {
-      tab = name_to_tab(args[2].clone());
+      tab = name_to_tab(options["preset"].as_str().unwrap().to_string());
     }
   }
 
-  if args.len() > 3 && !args[3].is_empty() {
-    demo_name = args[3].clone();
+  let mut demo_name = String::new();
+
+  if options.contains_key("demo") && !options["demo"].as_str().unwrap().is_empty() {
+    demo_name = options["demo"].as_str().unwrap().to_string();
   }
 
   let settings = crate::settings::load_settings();
 
-  crate::tf2::run_tf2(&demo_name, &settings, &tab);
-
-  false
-}
+  crate::tf2::run_tf2(&demo_name, &settings, &tab)
+} 
 
 fn name_to_tab(name: String) -> String {
   let settings = crate::settings::load_settings();
@@ -331,4 +492,23 @@ fn is_airshot(kill: &Value) -> bool {
   };
 
   killer && victim
+}
+
+pub fn run_cli() -> bool {
+  let args: Vec<String> = std::env::args().collect();
+
+  if args.len() < 2 {
+    return true;
+  }
+
+  let command = Commands::new(args);
+
+  if command.options().get("help").is_some() {
+    command.print_help();
+    return false;
+  }
+
+  command.run();
+
+  false
 }
